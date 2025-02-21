db5714acd6fd1563e9fcfa6db1c4bc35c2a3d546
Sugam Raut <sugamrautbim@gmail.com>
2025-02-21 22:27:30 +0545

    backend completed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,15 @@
 import express from 'express'
 import globalErrorHandler from './middlewares/globleErrorHangler'
 import noteRoute from './note/noteRoute'
+import cors from 'cors'
+import envConfig from './config/config'
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin:envConfig.frontend
+}))
+
 app.use("/api/notes",noteRoute)
 app.use(express.static("./src/uploads/"))
 
--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -4,7 +4,8 @@ config()
 const envConfig = {
     port : process.env.PORT, 
     mongodbString : process.env.MONGODB_URI,
-    environment:process.env.Node_ENV
+    environment:process.env.Node_ENV,
+    frontend:process.env.FRONTEND_URL
 }
 
 export default envConfig
